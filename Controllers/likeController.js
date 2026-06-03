6e914417dc33aa467f0f3eb817846e30011d5ac9
Purushottam Kumar <purushottam2fx@gmail.com>
2026-06-03 15:30:34 +0530

    add isLiked key in like model

--- Controllers/likeController.js
+++ Controllers/likeController.js
@@ -5,25 +5,33 @@ exports.toggleLike = async (req, res) => {
     const { postId } = req.params;
     const userId = req.user.id;
 
-    // Check if post exists
     const post = await Post.findByPk(postId);
     if (!post) return res.status(404).json({ message: "Post not found" });
 
-    // Check if already liked
     const existingLike = await Like.findOne({ where: { userId, postId } });
 
     if (existingLike) {
-      // Already liked, so Unlike it
       await existingLike.destroy();
+      const likesCount = await Like.count({ where: { postId } });
       return res
         .status(200)
-        .json({ success: true, message: "Post unliked successfully" });
+        .json({
+          success: true,
+          message: "Post unliked successfully",
+          isLiked: false,
+          likesCount: likesCount,
+        });
     } else {
-      // Not liked, so Like it
       await Like.create({ userId, postId });
+      const likesCount = await Like.count({ where: { postId } });
       return res
         .status(201)
-        .json({ success: true, message: "Post liked successfully" });
+        .json({
+          success: true,
+          message: "Post liked successfully",
+          isLiked: true,
+          likesCount: likesCount,
+        });
     }
   } catch (error) {
     res.status(500).json({ success: false, error: error.message });
